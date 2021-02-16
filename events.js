748c745b069305d5f78e241f09c5c967e90316d2
Zhanibek Seidzhan <zhann2013@gmail.com>
2021-02-16 23:50:03 +0600

    event.js over
    event.js over

--- events.js
+++ events.js
@@ -1,6 +1,13 @@
 const EventEmitter = require('events');
+const http = require('http');
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+    constructor(){
+        super();
+    }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", ()=>{
     console.log("There was a sale!");
@@ -15,3 +22,26 @@ myEmitter.on("newSale", stock=>{
 });
 
 myEmitter.emit("newSale", 9);
+
+//------------------------------------------
+
+
+const server = http.createServer();
+
+server.on('request', (req,res)=>{
+    console.log('Request recieved!');
+    res.end('Request recieved!');
+});
+
+server.on('request', (req,res)=>{
+    console.log('Another Request recieved!');
+    //res.end('Another Request recieved!');
+});
+
+server.on('close', ()=>{
+    console.log('Server closed!');
+});
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('Waiting for request... ');
+});
