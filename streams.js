32f1388b53caaf7ecc6987daaf973df3e3da6ce1
Zhanibek Seidzhan <zhann2013@gmail.com>
2021-02-18 22:54:54 +0600

    end of streams in node
    end of streams in node. begin tech bootcamp

---
+++ streams.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server
+    .on('request', (req,res)=>{
+
+        // solution 1
+
+        // fs.readFile('./test-file.txt', (err, data)=> {
+        //     if (err) console.log(err);
+        //     res.end(data);
+        // });
+
+        // solution 2
+
+        // const readable = fs.createReadStream('./test-file.txt');
+        // readable
+        //     .on( 'data', chunk => res.write(chunk) )
+        //     .on( 'end', ()=>res.end() )
+        //     .on( 'error', err=>{
+        //         console.log(err);
+        //         res.statusCode = 500;
+        //         res.end('File not found!')
+        //     });
+
+        // solution 3
+
+        const readable = fs.createReadStream('./test-file.txt');
+        readable.pipe(res);
+
+    })
+    .listen(8000, '127.0.0.1', ()=>{
+        console.log('Listening port 8000...')
+    });
